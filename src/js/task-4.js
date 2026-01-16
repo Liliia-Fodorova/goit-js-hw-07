bf51e36c0864dfab669f88b23445a3e8766df134
Liliia Fodorova <nevskay22@gmail.com>
2026-01-16 18:32:37 +0100

    add js

--- src/js/task-4.js
+++ src/js/task-4.js
@@ -1,1 +1,20 @@
+document.querySelector('.login-form').addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  const email = this.elements.email.value.trim();
+  const password = this.elements.password.value.trim();
+
+  if (email === '' || password === '') {
+    alert('All form fields must be filled in');
+    return;
+  }
+
+  const formData = {
+    email,
+    password,
+  };
+
+  console.log(formData);
+  this.reset();
+});
 
