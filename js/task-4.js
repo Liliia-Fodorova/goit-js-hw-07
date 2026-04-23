9cd08b43ac2c21fb50d5f60e7e898107fca2b632
Liliia Fodorova <nevskay22@gmail.com>
2026-04-23 23:11:48 +0200

    add file task-4.js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+document.querySelector("login-form").addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    const email = this.elemants.email.value.trim();
+    const password = this.elemants.password.value.trim();
+
+    if (email === '' || password === '') {
+        alert('All form fields must be filled in');
+        return;
+    };
+    const formData = {
+        email,
+        password,
+    };
+    console.log(formData);
+    this.reset();
+    
+});
 
